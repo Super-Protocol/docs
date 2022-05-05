57650d52cc2853c1030e89af31ae96b9fd72a7b4
Ilya Shmelev <ishmelev23@gmail.com>
2022-05-05 21:02:27 +0400

    Updated and fixed solc version

--- sources/contracts/gen-docs.sh
+++ sources/contracts/gen-docs.sh
@@ -4,4 +4,4 @@ set -e
 cd sources/contracts/repo
 yarn
 rm -rf ../../../docs/contracts
-solidity-docgen --solc-module solc-0.8.4 -o ../../../docs/contracts -t ../templates -H ../helpers.js
+solidity-docgen --solc-module solc-0.8.9 -o ../../../docs/contracts -t ../templates -H ../helpers.js
