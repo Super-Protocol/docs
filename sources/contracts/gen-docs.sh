807c6e4a631504af75a799a88e986dd145615bb0
Ilya Shmelev <ishmelev23@gmail.com>
2022-05-18 11:48:19 +0300

    Excluded libs

--- sources/contracts/gen-docs.sh
+++ sources/contracts/gen-docs.sh
@@ -5,3 +5,6 @@ cd sources/contracts/repo
 yarn
 rm -rf ../../../docs/contracts
 solidity-docgen --solc-module solc-0.8.9 -o ../../../docs/contracts -t ../templates -H ../helpers.js
+
+cd ../../../docs/contracts/
+rm -rf libs diamond mocks storages AppFacet.md
