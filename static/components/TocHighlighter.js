d4b6159a6495e2b7df44963a942129c6d05cd2ff
Evgeny <evgeny.sidorenko@gmail.com>
2023-10-27 10:39:32 +0300

    TOC

--- static/components/TocHighlighter.js
+++ static/components/TocHighlighter.js
@@ -2,20 +2,25 @@ export const onRouteDidUpdate = ({ location, previousLocation }) => {
   if (location.pathname === previousLocation?.pathname) return;
 
   const sections = Array.from(document.querySelectorAll("h2,h3"));
+  const items = Array.from(
+    document.querySelectorAll("a.table-of-contents__link")
+  );
+
   const scrollHandler = (entries) => {
-    entries.forEach((entry) => {
-      const section = entry.target;
-      const sectionId = section.id;
+    const firsSection = entries.filter(
+      (entry) => entry.intersectionRatio > 0
+    )[0];
+
+    if (firsSection) {
+      items.forEach((item) => {
+        item.classList?.remove("highlight");
+      });
+
+      const sectionId = firsSection.target.id;
       const sectionLink = document.querySelector(`a[href="#${sectionId}"]`);
 
-      if (sectionLink) {
-        if (entry.intersectionRatio > 0) {
-          sectionLink.classList?.add("highlight");
-        } else {
-          sectionLink.classList?.remove("highlight");
-        }
-      }
-    });
+      sectionLink.classList?.add("highlight");
+    }
   };
 
   const observer = new IntersectionObserver(scrollHandler);
